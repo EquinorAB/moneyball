a88edea59f1d2ea859dc9ba3ecd3b257e2f8fcca
EquinorAB <104419574+EquinorAB@users.noreply.github.com>
2020-10-21 05:43:55 +0000

    Run scan-local.sh on php-zendserver:...

--- e2e/client.cr
+++ e2e/client.cr
@@ -47,4 +47,52 @@ module ::E2E
 
       puts "Fast transaction percentage: #{pct_fast_txns}"
 
-      request = {call: 0, content: {node_ports: node_ports, num_miners: num_mi
+      request = {call: 0, content: {node_ports: node_ports, num_miners: num_miners, num_tps: num_tps, pct_fast_txns: pct_fast_txns}.to_json}.to_json
+      client.exec(request)
+    end
+
+    def self.launch
+      request = {call: 1, content: ""}.to_json
+      client.exec(request)
+    end
+
+    def self.finish
+      request = {call: 2, content: ""}.to_json
+      client.exec(request)
+    end
+
+    def task(message : String)
+      work = ClientWork.from_json(message)
+
+      case work[:call]
+      when 0 # initialize
+        initialize = Initialize.from_json(work[:content])
+
+        @node_ports = initialize.node_ports
+        @num_miners = initialize.num_miners
+        @num_tps = initialize.num_tps
+        @pct_fast_txns = initialize.pct_fast_txns
+      when 1 # launch
+        launch
+      when 2 # finish
+        kill
+
+        response({num_transactions: num_transactions, duration: duration}.to_json)
+      end
+    end
+
+    def self.receive
+      client.receive
+    end
+
+    @transaction_ids = [] of String
+
+    @alive : Bool = true
+
+    @node_ports : Array(Int32) = [] of Int32
+    @num_miners : Int32 = 0
+    @num_tps : Int32 = 0
+
+    @pct_fast_txns : Int32 = 1
+
+    def create_transaction(doing_fast_tra
