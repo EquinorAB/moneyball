3ecf04ce55dcf8a9f6b830159757eac7de36258a
EquinorAB <104419574+EquinorAB@users.noreply.github.com>
2020-10-23 20:23:54 +0000

    changed readme.md

--- e2e/runner.cr
+++ e2e/runner.cr
@@ -27,4 +27,36 @@ module ::E2E
     @db_name : String
 
     @node_ports : Array(Int32)
-    @node_ports_public
+    @node_ports_public : Array(Int32) = [] of Int32
+
+    @num_transactions : Int32 = 0
+    @duration : Float64 = 0.0
+
+    getter exit_code : Int32 = 0
+
+    def initialize(@mode : Int32, @num_nodes : Int32, @num_miners : Int32, @time : Int32, @keep_logs : Bool, @no_transactions : Bool, @num_tps : Int32, @pct_fast_txns : Int32)
+      @node_ports = (4001..4001 + (@num_nodes - 1)).to_a
+
+      Client.initialize(@node_ports, @num_miners, @no_transactions, @num_tps, @pct_fast_txns)
+
+      @db_name = Random.new.hex
+    end
+
+    def create_wallets_and_funds
+      wallet_addresses = [] of String
+      [@num_nodes, @num_miners].max.times do |idx|
+        create_wallet(idx)
+        wallet_json = File.read(wallet(idx))
+        the_parsed_wallet = JSON.parse(wallet_json)
+        address = the_parsed_wallet["address"]
+        wallet_addresses << address.as_s
+      end
+      developer_fund_string = "addresses:\n"
+      wallet_addresses.each do |addr|
+        developer_fund_string += "  - address: #{addr}\n    amount: \"10000\"\n"
+      end
+      File.write(developer_fund_file, developer_fund_string, "w")
+      create_official_nodes(wallet_addresses)
+    end
+
+    def create_official_nodes(wallet_addresses : Arr
