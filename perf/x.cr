bdc82fbe5db3a516a5d2521869c7314316ffbb94
EquinorAB <104419574+EquinorAB@users.noreply.github.com>
2020-11-06 07:24:27 +0000

    - ‘paper/.#TIR.org’ / ‘paper/TIR.org’

--- perf/x.cr
+++ perf/x.cr
@@ -3,4 +3,39 @@
 # See the LICENSE file at the top-level directory of this distribution
 # for licensing information.
 #
-# Unless otherwise agreed in a custom licensing agreement with the Axentr
+# Unless otherwise agreed in a custom licensing agreement with the Axentro Core developers,
+# no part of this software, including this file, may be copied, modified,
+# propagated, or distributed except according to the terms contained in the
+# LICENSE file.
+#
+# Removal or modification of this copyright notice is prohibited.
+
+require "json"
+require "random"
+require "base64"
+
+require "/../src/core.cr"
+require "/../src/core/keys/wif.cr"
+
+class X
+  def self.create_id : String
+    tmp_id = Random::Secure.hex(32)
+    return create_id if tmp_id[0] == "0"
+    tmp_id
+  end
+
+  def self.transaction(from_wallet, to_address, iterations = 10)
+    File.open("txns.txt", "w") { |f|
+      (1..iterations).to_a.each do |_|
+        path = File.expand_path(from_wallet, __FILE__)
+        wallet_json = File.read(path)
+        w = JSON.parse(wallet_json)
+
+        id = create_id
+        from_address = w["address"].as_s
+        public_key = w["public_key"].as_s
+        wif_string = w["wif"].as_s
+        wif = Axentro::Core::Keys::Wif.new(wif_string)
+        private_key = wif.private_key.as_hex
+
+        unsigned_transaction = %Q({"id":"#{id}","action":"send","senders":[{"address":"#{from_address}","public_key":"#{public_key}","amount":100000000,"fee":10000,"signature":"0"}],"recipients":[{"address":"#{to_address}
