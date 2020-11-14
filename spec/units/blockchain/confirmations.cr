219ef468046c6a967f3296530eaf4ec19fe97346
EquinorAB <104419574+EquinorAB@users.noreply.github.com>
2020-11-14 05:46:25 +0000

    Add MIT license

--- spec/units/blockchain/confirmations.cr
+++ spec/units/blockchain/confirmations.cr
@@ -157,4 +157,44 @@ describe Blockchain do
         when 4
           res[:block].index.should eq(5)
         when 5
-         
+          res[:block].index.should eq(6)
+        when 6
+          res[:block].index.should eq(3)
+        when 7
+          res[:block].index.should eq(4)
+        when 8
+          res[:block].index.should eq(1)
+        when 9
+          res[:block].index.should eq(2)
+        when 10
+          res[:block].index.should eq(0)
+        end
+
+        case res[:confirmations]
+        when 0
+          res[:block].index.should eq(9)
+        when 1
+          res[:block].index.should eq(10)
+        when 2
+          res[:block].index.should eq(7)
+        when 3
+          res[:block].index.should eq(8)
+        when 4
+          res[:block].index.should eq(5)
+        when 5
+          res[:block].index.should eq(6)
+        when 6
+          res[:block].index.should eq(3)
+        when 7
+          res[:block].index.should eq(4)
+        when 8
+          res[:block].index.should eq(1)
+        when 9
+          res[:block].index.should eq(2)
+        when 10
+          res[:block].index.should eq(0)
+        end
+      end
+    end
+  end
+end
---
+++ spec/units/blockchain/consensus.cr
@@ -0,0 +1,47 @@
+
+# Copyright © 2017-2020 The Axentro Core developers
+#
+# See the LICENSE file at the top-level directory of this distribution
+# for licensing information.
+#
+# Unless otherwise agreed in a custom licensing agreement with the Axentro Core developers,
+# no part of this software, including this file, may be copied, modified,
+# propagated, or distributed except according to the terms contained in the
+# LICENSE file.
+#
+# Removal or modification of this copyright notice is prohibited.
+
+require "./../../spec_helper"
+require "./../../../src/cli/modules/logger"
+
+include Axentro::Core
+include TransactionModels
+include Axentro::Core::Consensus
+include Axentro::Interface::Logger
+include Units::Utils
+
+describe Consensus do
+  describe "calculate_pow_difficulty" do
+    it "should return a valid difficulty value" do
+      ENV.delete("AX_SET_DIFFICULTY")
+      hash = "e33598878258c85c60c5c29a41ad756cb69ecd315154695970c375a792eb8f4e"
+      nonce = "17186779519462547558"
+      calculate_pow_difficulty(MiningVersion::V1, hash, nonce, 13).should eq(13)
+    end
+  end
+
+  describe "is_nonce_valid?" do
+    it "when valid" do
+      ENV.delete("AX_SET_DIFFICULTY")
+      hash = "e33598878258c85c60c5c29a41ad756cb69ecd315154695970c375a792eb8f4e"
+      nonce = "17186779519462547558"
+      is_nonce_valid?(MiningVersion::V1, hash, nonce, 13).should eq(true)
+    end
+    it "when invalid" do
+      ENV.delete("AX_SET_DIFFICULTY")
+      hash = "e33598878258c85c60c5c29a41ad756cb69ecd315154695970c375a792eb8f4e"
+      nonce = "17186779519462547559"
+      is_nonce_valid?(MiningVersion::V1, hash, nonce, 13).should eq(false)
+    end
+  end
+end
