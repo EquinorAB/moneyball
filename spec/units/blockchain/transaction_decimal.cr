3026676b1f37e2a03d59b5a0e814a0e07e4e387f
EquinorAB <104419574+EquinorAB@users.noreply.github.com>
2020-11-20 06:21:57 +0000

    [MERGE] merge from trunk addons

--- spec/units/blockchain/transaction_decimal.cr
+++ spec/units/blockchain/transaction_decimal.cr
@@ -26,4 +26,38 @@ describe TransactionDecimal do
     transaction = TransactionDecimal.new(
       transaction_id,
       "send", # action
- 
+      [a_decimal_sender(sender_wallet, "1000000")],
+      [a_decimal_recipient(recipient_wallet, "1000000")],
+      [] of Transaction::Asset,
+      [] of Transaction::Module,
+      [] of Transaction::Input,
+      [] of Transaction::Output,
+      "",            # linked
+      "0",           # message
+      TOKEN_DEFAULT, # token
+      "0",           # prev_hash
+      0_i64,         # timestamp
+      0,             # scaled
+      TransactionKind::SLOW,
+      TransactionVersion::V1
+    )
+
+    transaction.action.should eq("send")
+    senders = transaction.senders
+    senders.size.should eq(1)
+    senders.first.address.should eq(sender_wallet.address)
+    senders.first.public_key.should eq(sender_wallet.public_key)
+    senders.first.amount.should eq("1000000")
+
+    recipients = transaction.recipients
+    recipients.size.should eq(1)
+    recipients.first.address.should eq(recipient_wallet.address)
+    recipients.first.amount.should eq("1000000")
+
+    transaction.id.should eq(transaction_id)
+    transaction.message.should eq("0")
+    transaction.prev_hash.should eq("0")
+  end
+
+  it "should raise scaled error on create if unscaled" do
+    sender_wallet = Wallet.from_json(Wallet.create(t
