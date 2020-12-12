8d29116cc7467d5af7f6a088a8555678900832db
EquinorAB <104419574+EquinorAB@users.noreply.github.com>
2020-12-12 21:05:27 +0000

    [doc]

--- spec/units/dapps/assets.cr
+++ spec/units/dapps/assets.cr
@@ -648,4 +648,27 @@ describe AssetComponent do
             "update_asset",
             [a_sender(sender_wallet, 0_i64, 0_i64)],
             [a_recipient(sender_wallet, 0_i64)],
-            [Transaction::Asset.new(asset_id_1, "name", "description", "media_location", "media_hash", 1, "terms", Asse
+            [Transaction::Asset.new(asset_id_1, "name", "description", "media_location", "media_hash", 1, "terms", AssetAccess::UNLOCKED, 2, __timestamp)]
+          )
+          transaction3 = transaction_factory.make_asset(
+            "AXNT",
+            "create_asset",
+            [a_sender(sender_wallet, 0_i64, 0_i64)],
+            [a_recipient(sender_wallet, 0_i64)],
+            [Transaction::Asset.new(asset_id_2, "name", "description", "media_location", "media_hash", 1, "terms", AssetAccess::UNLOCKED, 1, __timestamp)]
+          )
+          block_factory.add_slow_blocks(10)
+          component = AssetComponent.new(block_factory.blockchain)
+
+          result = component.valid_transactions?([transaction1, transaction2, transaction3])
+          result.passed.size.should eq(2)
+          result.failed.size.should eq(1)
+          result.failed.first.reason.should eq("asset media_location must not already exist (asset_id: #{asset_id_2}, media_location: media_location) 'create_asset'")
+        end
+      end
+
+      it "media_location should not exist unless it's for the asset that is being updated (in db)" do
+        with_factory do |block_factory, transaction_factory|
+          sender_wallet = transaction_factory.sender_wallet
+          asset_id_1 = Transaction::Asset.create_id
+  
