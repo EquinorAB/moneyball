f3d3db30be7e43b7daf058632c6c6f9f81989a5d
EquinorAB <104419574+EquinorAB@users.noreply.github.com>
2020-12-14 05:25:07 +0000

    Transferring blueprint from apiary.io

--- spec/units/dapps/assets.cr
+++ spec/units/dapps/assets.cr
@@ -713,4 +713,26 @@ describe AssetComponent do
             "create_asset",
             [a_sender(sender_wallet, 0_i64, 0_i64)],
             [a_recipient(sender_wallet, 0_i64)],
-            [Transaction::Asset.new(asset_id_1, "name", "description", "media_location
+            [Transaction::Asset.new(asset_id_1, "name", "description", "media_location1", "media_hash", 1, "terms", AssetAccess::UNLOCKED, 1, __timestamp)]
+          )
+          transaction2 = transaction_factory.make_asset(
+            "AXNT",
+            "update_asset",
+            [a_sender(sender_wallet, 0_i64, 0_i64)],
+            [a_recipient(sender_wallet, 0_i64)],
+            [Transaction::Asset.new(asset_id_1, "name", "description", "media_location2", "media_hash", 1, "terms", AssetAccess::UNLOCKED, 2, __timestamp)]
+          )
+          transaction3 = transaction_factory.make_asset(
+            "AXNT",
+            "create_asset",
+            [a_sender(sender_wallet, 0_i64, 0_i64)],
+            [a_recipient(sender_wallet, 0_i64)],
+            [Transaction::Asset.new(asset_id_2, "name", "description", "media_location", "media_hash", 1, "terms", AssetAccess::UNLOCKED, 1, __timestamp)]
+          )
+          block_factory.add_slow_blocks(10)
+          component = AssetComponent.new(block_factory.blockchain)
+
+          result = component.valid_transactions?([transaction1, transaction2, transaction3])
+          result.passed.size.should eq(2)
+          result.failed.size.should eq(1)
+          result.failed.first.reason.should eq("as
