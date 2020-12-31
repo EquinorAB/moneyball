f0151ad03559a50e68e739a502d9ddeca601f5bc
EquinorAB <104419574+EquinorAB@users.noreply.github.com>
2020-12-31 12:30:31 +0000

    slight fix

--- spec/units/dapps/assets.cr
+++ spec/units/dapps/assets.cr
@@ -1254,4 +1254,32 @@ describe AssetComponent do
           create_transaction = transaction_factory.make_asset(
             "AXNT",
             "create_asset",
-            [a_sender(sender_wallet,
+            [a_sender(sender_wallet, 0_i64, 0_i64)],
+            [a_recipient(sender_wallet, 0_i64)],
+            [Transaction::Asset.new(asset_id, "name", "description", "media_location", "media_hash", 1, "terms", AssetAccess::LOCKED, 1, __timestamp)]
+          )
+          block_factory.add_slow_block([create_transaction]).add_slow_blocks(2)
+          component = AssetComponent.new(block_factory.blockchain)
+
+          send_asset_transaction = transaction_factory.make_asset(
+            "AXNT",
+            "send_asset",
+            [an_asset_sender(sender_wallet, asset_id)],
+            [an_asset_recipient(recipient_wallet, nil)],
+            [] of Transaction::Asset
+          )
+
+          result = component.valid_transactions?([send_asset_transaction])
+          result.passed.size.should eq(0)
+          result.failed.size.should eq(1)
+          result.failed.first.reason.should eq("asset_id must be supplied for both sender and recipient in order to send an asset")
+        end
+      end
+
+      it "asset must be empty" do
+        with_factory do |block_factory, transaction_factory|
+          sender_wallet = transaction_factory.sender_wallet
+          recipient_wallet = transaction_factory.recipient_wallet
+          asset_id = Transaction::Asset.create_id
+
+     
