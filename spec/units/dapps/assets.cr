58b083e1bc07a870fe7bb26bca62000ff65e9ffa
EquinorAB <104419574+EquinorAB@users.noreply.github.com>
2021-01-01 14:06:10 +0000

    sched: Fix SCHED_HRTICK bug leading to late preemption of tasks

--- spec/units/dapps/assets.cr
+++ spec/units/dapps/assets.cr
@@ -1282,4 +1282,25 @@ describe AssetComponent do
           recipient_wallet = transaction_factory.recipient_wallet
           asset_id = Transaction::Asset.create_id
 
-     
+          create_transaction = transaction_factory.make_asset(
+            "AXNT",
+            "create_asset",
+            [a_sender(sender_wallet, 0_i64, 0_i64)],
+            [a_recipient(sender_wallet, 0_i64)],
+            [Transaction::Asset.new(asset_id, "name", "description", "media_location", "media_hash", 1, "terms", AssetAccess::LOCKED, 1, __timestamp)]
+          )
+          block_factory.add_slow_block([create_transaction]).add_slow_blocks(2)
+          component = AssetComponent.new(block_factory.blockchain)
+
+          send_asset_transaction = transaction_factory.make_asset(
+            "AXNT",
+            "send_asset",
+            [an_asset_sender(sender_wallet, asset_id)],
+            [an_asset_recipient(recipient_wallet, asset_id)],
+            [Transaction::Asset.new(asset_id, "name", "description", "media_location", "media_hash", 1, "terms", AssetAccess::LOCKED, 1, __timestamp)]
+          )
+
+          result = component.valid_transactions?([send_asset_transaction])
+          result.passed.size.should eq(0)
+          result.failed.size.should eq(1)
+          result.failed.first.reason.should eq("The assets should be empty in the transaction for action
