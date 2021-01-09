e809326316786b499b200177e6ae5743ee6410cb
EquinorAB <104419574+EquinorAB@users.noreply.github.com>
2021-01-09 18:04:54 +0000

    remove version

--- spec/units/dapps/assets.cr
+++ spec/units/dapps/assets.cr
@@ -1592,4 +1592,30 @@ describe AssetComponent do
             "AXNT",
             "send_asset",
             [an_asset_sender(sender_wallet, asset_id, 1)],
-            [an_asset_recipient(recipient_wallet, asset_i
+            [an_asset_recipient(recipient_wallet, asset_id, 1)],
+            [] of Transaction::Asset
+          )
+
+          result = component.valid_transactions?([create_transaction, send_asset_transaction, send_asset_transaction_2])
+          result.passed.size.should eq(2)
+          result.failed.size.should eq(1)
+          result.failed.first.reason.should eq("you have 0 quantity of asset: #{asset_id} so you cannot send 1")
+        end
+      end
+
+      it "cannot send an asset quantity if you don't have enough after sending once already (in db)" do
+        with_factory do |block_factory, transaction_factory|
+          sender_wallet = transaction_factory.sender_wallet
+          recipient_wallet = transaction_factory.recipient_wallet
+
+          asset_id = Transaction::Asset.create_id
+
+          create_transaction = transaction_factory.make_asset(
+            "AXNT",
+            "create_asset",
+            [a_sender(sender_wallet, 0_i64, 0_i64)],
+            [a_recipient(sender_wallet, 0_i64)],
+            [Transaction::Asset.new(asset_id, "name", "description", "media_location", "media_hash", 1, "terms", AssetAccess::LOCKED, 1, __timestamp)]
+          )
+
+          send_asset_transaction = transaction_factory.m
