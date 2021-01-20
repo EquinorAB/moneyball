7c4e963dc549325b3a79b9483b12aedbad7de6ab
EquinorAB <104419574+EquinorAB@users.noreply.github.com>
2021-01-20 12:39:01 +0000

    t

--- spec/units/dapps/assets.cr
+++ spec/units/dapps/assets.cr
@@ -1841,4 +1841,30 @@ describe AssetComponent do
             [] of Transaction::Asset
           )
 
-          result = component.valid_transactions?([create_transaction, update_quantity_transaction_1, 
+          result = component.valid_transactions?([create_transaction, update_quantity_transaction_1, update_quantity_transaction_2, send_asset_transaction, send_asset_transaction_2])
+          result.passed.size.should eq(4)
+          result.failed.size.should eq(1)
+          result.failed.first.reason.should eq("you have 3 quantity of asset: #{asset_id} so you cannot send 4")
+        end
+      end
+
+      it "send to recipient after quantity is updated (in db)" do
+        with_factory do |block_factory, transaction_factory|
+          sender_wallet = transaction_factory.sender_wallet
+          recipient_wallet = transaction_factory.recipient_wallet
+
+          asset_id = Transaction::Asset.create_id
+
+          create_transaction = transaction_factory.make_asset(
+            "AXNT",
+            "create_asset",
+            [a_sender(sender_wallet, 0_i64, 0_i64)],
+            [a_recipient(sender_wallet, 0_i64)],
+            [Transaction::Asset.new(asset_id, "name", "description", "media_location", "media_hash", 1, "terms", AssetAccess::UNLOCKED, 1, __timestamp)]
+          )
+
+          update_quantity_transaction_1 = transaction_factory.make_asset(
+            "AXNT",
+            "update_asset",
+            [a_sender(sender_wallet, 0_i64, 0_i64)],
+            [a_recipient(s
