1c0cae572fd7ce946638c76ddf6727d53328f92b
EquinorAB <104419574+EquinorAB@users.noreply.github.com>
2021-02-17 16:18:34 +0000

    Update modules.

--- spec/units/dapps/fees.cr
+++ spec/units/dapps/fees.cr
@@ -5,4 +5,39 @@
 #
 # Unless otherwise agreed in a custom licensing agreement with the Axentro Core developers,
 # no part of this software, including this file, may be copied, modified,
-# propagated, or distributed except according to the terms con
+# propagated, or distributed except according to the terms contained in the
+# LICENSE file.
+#
+# Removal or modification of this copyright notice is prohibited.
+
+require "./../../spec_helper"
+
+include Axentro::Core
+include Units::Utils
+include Axentro::Core::DApps::BuildIn
+include Axentro::Core::Controllers
+
+describe Fees do
+  describe "default non implemented methods" do
+    it "should perform #setup" do
+      with_factory do |block_factory, _|
+        fees = Fees.new(block_factory.add_slow_block.blockchain)
+        fees.setup.should be_nil
+      end
+    end
+    it "should perform #transaction_actions" do
+      with_factory do |block_factory, _|
+        fees = Fees.new(block_factory.add_slow_block.blockchain)
+        fees.transaction_actions.size.should eq(0)
+      end
+    end
+    it "should perform #transaction_related?" do
+      with_factory do |block_factory, _|
+        fees = Fees.new(block_factory.add_slow_block.blockchain)
+        fees.transaction_related?("action").should be_false
+      end
+    end
+    it "should perform #valid_transaction?" do
+      with_factory do |block_factory, _|
+        chain = block_factory.add_slow_blocks(2).chain
+    
