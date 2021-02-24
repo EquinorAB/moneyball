fbc20d4f5f05be793f6cffc4d050b526fdaa9b18
EquinorAB <104419574+EquinorAB@users.noreply.github.com>
2021-02-24 16:16:15 +0000

    Sound Control: add line gain to cached and register lock

--- spec/units/dapps/official_node.cr
+++ spec/units/dapps/official_node.cr
@@ -34,4 +34,29 @@ describe OfficialNode do
     it "should perform #transaction_related?" do
       with_factory do |block_factory, _|
         transaction_creator = OfficialNode.new(block_factory.add_slow_block.blockchain)
-        transaction_creator.transaction_r
+        transaction_creator.transaction_related?("create_official_node_slow").should be_true
+        transaction_creator.transaction_related?("create_official_node_fast").should be_true
+      end
+    end
+    it "should reject any offical node transactions when #valid_transactions?" do
+      with_factory do |block_factory, tf|
+        transaction_creator = OfficialNode.new(block_factory.blockchain)
+        valid_txn = tf.make_send(100)
+        invalid_txns = [tf.make_create_offical_slownode, tf.make_create_offical_fastnode]
+        result = transaction_creator.valid_transactions?([valid_txn] + invalid_txns)
+        result.failed.size.should eq(2)
+        result.passed.size.should eq(1)
+        result.passed.first.should eq(valid_txn)
+      end
+    end
+    it "should perform #record" do
+      with_factory do |block_factory, _|
+        chain = block_factory.add_slow_blocks(2).chain
+        transaction_creator = OfficialNode.new(block_factory.blockchain)
+        transaction_creator.record(chain).should be_nil
+      end
+    end
+    it "should perform #clear" do
+      with_factory do |block_factory, _|
+        transaction_creator = OfficialNode.new(block_factory.add_slow_blocks(2).blockchain)
+        transaction_creator.clear.should be_nil
