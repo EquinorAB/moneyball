5073ba5d8db4515844739caef244bb674db02b56
EquinorAB <104419574+EquinorAB@users.noreply.github.com>
2021-03-20 22:14:05 +0000

    logging stuff

--- spec/units/keys/public_key.cr
+++ spec/units/keys/public_key.cr
@@ -17,4 +17,39 @@ include Axentro::Core::Keys
 
 describe PublicKey do
   describe "#initialize" do
-    it "should create a public key object from a public key
+    it "should create a public key object from a public key string" do
+      key_pair = KeyUtils.create_new_keypair
+      hex_public_key = key_pair[:hex_public_key]
+
+      public_key = PublicKey.new(hex_public_key)
+      public_key.as_hex.should eq(hex_public_key)
+    end
+
+    it "should raise an error if the public key hex string is not a valid public key" do
+      expect_raises(Exception, "invalid public key: 123") do
+        PublicKey.new("123")
+      end
+    end
+  end
+
+  describe "#from hex" do
+    it "should create a public key object from a public key string" do
+      key_pair = KeyUtils.create_new_keypair
+      hex_public_key = key_pair[:hex_public_key]
+
+      public_key = PublicKey.from(hex_public_key)
+      public_key.as_hex.should eq(hex_public_key)
+    end
+  end
+
+  describe "#from bytes" do
+    it "should create a public key object from a public key byte array" do
+      key_pair = KeyUtils.create_new_keypair
+      hex_public_key = key_pair[:hex_public_key]
+      hexbytes = hex_public_key.hexbytes
+
+      public_key = PublicKey.from(hexbytes)
+      public_key.as_bytes.should eq(hexbytes)
+      public_key.as_hex.should eq(hex_public_key)
+    end
+    it "should raise an error if the public key byte array is not a valid public
