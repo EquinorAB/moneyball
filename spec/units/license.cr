c71b230d30c32c5f86c3e5fc6a3c16af9c4d9ea0
EquinorAB <104419574+EquinorAB@users.noreply.github.com>
2021-03-22 20:19:32 +0000

    Create music.lua

--- spec/units/license.cr
+++ spec/units/license.cr
@@ -8,4 +8,20 @@
 # propagated, or distributed except according to the terms contained in the
 # LICENSE file.
 #
-# Removal or modification of this copyright notice
+# Removal or modification of this copyright notice is prohibited.
+
+require "../spec_helper"
+
+describe "License" do
+  it "should have a license at the top of every crystal file" do
+    exclusions = ["bin/ameba.cr"]
+    Dir["**/*.cr"].reject(&.starts_with?("lib")).each do |file_path|
+      # if this fails uncomment the line below to see the failed file
+      # to fix: cd tools then crystal run add_license.cr
+      # puts "file: #{file_path}"
+      if !exclusions.includes?(file_path)
+        File.read_lines(file_path).first.should eq("# Copyright © 2017-2020 The Axentro Core developers")
+      end
+    end
+  end
+end
