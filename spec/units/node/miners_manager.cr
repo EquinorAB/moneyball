b2789219976dd63073ec9ded768a5cdf72706580
EquinorAB <104419574+EquinorAB@users.noreply.github.com>
2021-03-23 15:35:32 +0000

    merging changes

--- spec/units/node/miners_manager.cr
+++ spec/units/node/miners_manager.cr
@@ -18,4 +18,39 @@ include Axentro::Core::NodeComponents
 
 ONE_SECOND = 1000
 
-describe Min
+describe MinersManager do
+  it "should include in ban list if duration between join and remove is less than 10 seconds for 10 times" do
+    now = __timestamp
+    miner_mortality = LRUCache(String, Array(MinerMortality)).new(max_size: 10_000)
+
+    (1..11).to_a.each do |_|
+      join_and_remove(miner_mortality, now, ONE_SECOND)
+      now += ONE_SECOND
+    end
+
+    MinersManager.ban_list(miner_mortality.items).should eq(Set{"1"})
+  end
+
+  it "should not include in ban list if duration between join and remove is greater than 10 seconds for 10 times" do
+    now = __timestamp
+    miner_mortality = LRUCache(String, Array(MinerMortality)).new(max_size: 10_000)
+
+    # add 9 that are less than boundary
+    (1..9).to_a.each do |_|
+      join_and_remove(miner_mortality, now, ONE_SECOND * 11)
+      now += ONE_SECOND
+    end
+
+    # add 10 that are greater then boundary
+    (1..10).to_a.each do |_|
+      join_and_remove(miner_mortality, now, ONE_SECOND * 11)
+    end
+
+    # should not be in ban list because only 9 happened less than boundary
+    MinersManager.ban_list(miner_mortality.items).should be_empty
+  end
+end
+
+def join_and_remove(miner_mortality, now, spacing)
+  mortalities = miner_mortality.get("1") || [] of MinerMortality
+  mortalities << M
