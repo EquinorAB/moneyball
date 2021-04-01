1423e716b9165341ea1a8c0a185ffb687eabc8c4
EquinorAB <104419574+EquinorAB@users.noreply.github.com>
2021-04-01 10:03:02 +0000

    Relicense to MIT

--- spec/units/official_nodes/official_nodes.cr
+++ spec/units/official_nodes/official_nodes.cr
@@ -37,4 +37,29 @@ describe OfficialNodes do
 
   it "should return the config" do
     on = OfficialNodes.new("#{__DIR__}/../../utils/data/official_nodes.yml")
-    on.get_config.should eq({"fastnodes" => ["VDBkMzRmOWZlYWEwYmM4OWY4MjZhNDlmZThhNTY1MmI3NzZjYTNkZjVlNzYzMjZ
+    on.get_config.should eq({"fastnodes" => ["VDBkMzRmOWZlYWEwYmM4OWY4MjZhNDlmZThhNTY1MmI3NzZjYTNkZjVlNzYzMjZi"], "slownodes" => ["VDAyNThiOWFiN2Q5YWM3ZjUyYTNhYzQwZTY1NDBmYWJkMjczZmVmZThlOTgzMWM4"]})
+  end
+
+  it "should make official node transactions with a coinbase when no developer fund is present" do
+    on = OfficialNodes.new("#{__DIR__}/../../utils/data/official_nodes.yml")
+    transactions = OfficialNodes.transactions(on.get_config, [] of Axentro::Core::Transaction)
+
+    coinbase = transactions.select(&.is_coinbase?)
+    coinbase.size.should eq(1)
+
+    official_node_slow = transactions.select(&.action.==("create_official_node_slow"))
+    official_node_slow.size.should eq(1)
+
+    official_node_fast = transactions.select(&.action.==("create_official_node_slow"))
+    official_node_fast.size.should eq(1)
+
+    transactions[0].prev_hash.should eq("0")
+    transactions[1].prev_hash.should eq(transactions[0].to_hash)
+    transactions[2].prev_hash.should eq(transactions[1].to_hash)
+  end
+
+  it "should make official node transactions with a coinbase when developer fund is present" do
+    df = DeveloperFund.validate("#{__DIR__}/../../utils/data/developer_fund.yml")
+    fund_transactions = DeveloperFund.transactions(df.not_nil!.get_config)
+
+    on = OfficialN
