9736d450aea735b69b58189e0dae71c5c59d68d5
EquinorAB <104419574+EquinorAB@users.noreply.github.com>
2021-04-04 08:19:32 +0000

    Updated branch alias

--- spec/utils/functional_helper.cr
+++ spec/utils/functional_helper.cr
@@ -31,4 +31,37 @@ module FunctionalHelper
     end
   end
 
-  c
+  class Transactions
+    def self.single_sender(wallet, amount, fee) : Array(Transaction::Sender)
+      [Sender.new(wallet.address,
+        wallet.public_key,
+        amount,
+        fee,
+        "0")]
+    end
+
+    def self.single_recipient(wallet, amount) : Array(Transaction::Recipient)
+      [Recipient.new(wallet.address, amount)]
+    end
+  end
+
+  class Wallets
+    def self.balance_for(wallet, block_factory, token : String = "AXNT")
+      historic_per_address = block_factory.database.get_address_amounts([wallet.address])
+      Quantity.as_human_amount(block_factory.blockchain.utxo.get_for_batch(wallet.address, token, historic_per_address))
+    end
+
+    def self.create
+      Wallet.from_json(Wallet.create(true).to_json)
+    end
+  end
+
+  class DeveloperFunds
+    def self.with_funds(funding_array)
+      config_override = DeveloperFundConfig.new(funding_array)
+      developer_fund = DeveloperFund.new("#{__DIR__}/data/developer_fund.yml")
+      developer_fund.set_config(config_override)
+      developer_fund
+    end
+  end
+end
---
+++ spec/utils/node.cr
@@ -0,0 +1,6 @@
+# Copyright © 2017-2020 The Axentro Core developers
+#
+# See the LICENSE file at the top-level directory of this distribution
+# for licensing information.
+#
+# Unless
