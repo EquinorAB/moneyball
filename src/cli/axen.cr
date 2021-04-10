8708d805e8f167e236234f29166fc84f475cd5c9
EquinorAB <104419574+EquinorAB@users.noreply.github.com>
2021-04-10 07:21:06 +0000

    First commmit

--- src/cli/axen.cr
+++ src/cli/axen.cr
@@ -67,3 +67,75 @@ module ::Axentro::Interface::Axen
         public_uri = URI.parse(public_url)
 
         puts_help(HELP_PUBLIC_URL) unless public_host = public_uri.host
+        puts_help(HELP_PUBLIC_URL) unless public_port = public_uri.port
+
+        ssl = (public_uri.scheme == "https")
+      end
+
+      has_first_connection = false
+      use_ssl = false
+
+      if connect_node = G.op.__connect_node
+        connect_uri = URI.parse(connect_node)
+        use_ssl = (connect_uri.scheme == "https")
+        has_first_connection = !connect_uri.host.nil?
+      end
+
+      wallet = nil
+      wallet_address = nil
+      if G.op.__fastnode
+        puts_help(HELP_WALLET_PATH) unless wallet_path = G.op.__wallet_path
+        wallet = get_wallet(wallet_path, G.op.__wallet_password)
+        wallet_address = wallet.address
+      else
+        if wallet_path = G.op.__wallet_path
+          wallet = get_wallet(wallet_path, G.op.__wallet_password)
+          wallet_address = wallet.address
+        else
+          puts_help(HELP_NODE_ADDRESS) if G.op.__address.nil?
+
+          wallet_address = G.op.__address.not_nil!
+        end
+      end
+
+      database = if database_path = G.op.__database_path
+                   Core::Database.new(database_path)
+                 else
+                   raise "A database is required for node startup (use -d option)"
+                 end
+
+      developer_fund = G.op.__developer_fund
+      official_nodes = G.op.__official_nodes
+
+      security_level_percentage = G.op.__security_level_percentage
+      sync_chunk_size = G.op.__sync_chunk_size
+
+      max_miners = G.op.__max_miners
+      max_nodes = G.op.__max_nodes
+      record_nonces = G.op.__record_nonces
+
+      whitelist = G.op.__whitelist
+      whitelist_message = G.op.__whitelist_message
+      metrics_whitelist = G.op.__metrics_whitelist
+
+      connection_port = get_connecting_port(use_ssl)
+
+      node = if has_first_connection
+               Core::Node.new(G.op.__is_private, G.op.__is_testnet, G.op.__bind_host, G.op.__bind_port, public_host, public_port, ssl, connect_uri.not_nil!.host, connection_port, wallet, wallet_address, database_path, database, developer_fund, official_nodes, G.op.__exit_if_unofficial, security_level_percentage, sync_chunk_size, record_nonces, max_miners, max_nodes, whitelist, whitelist_message, metrics_whitelist, use_ssl)
+             else
+               Core::Node.new(G.op.__is_private, G.op.__is_testnet, G.op.__bind_host, G.op.__bind_port, public_host, public_port, ssl, nil, nil, wallet, wallet_address, database_path, database, developer_fund, official_nodes, G.op.__exit_if_unofficial, security_level_percentage, sync_chunk_size, record_nonces, max_miners, max_nodes, whitelist, whitelist_message, metrics_whitelist, use_ssl)
+             end
+      node.run!
+    end
+
+    include Core::Protocol
+    include Logger
+  end
+end
+
+include ::Axentro::Interface
+include Axentro::Core::Keys
+
+::Axentro::Interface::Axen::Root.new(
+  {name: "axen", desc: "Axentro node"}, [] of AxeAction
+).run
---
+++ src/cli/helps/helps.cr
@@ -0,0 +1,45 @@
+
+# Copyright © 2017-2020 The Axentro Core developers
+#
+# See the LICENSE file at the top-level directory of this distribution
+# for licensing information.
+#
+# Unless otherwise agreed in a custom licensing agreement with the Axentro Core developers,
+# no part of this software, including this file, may be copied, modified,
+# propagated, or distributed except according to the terms contained in the
+# LICENSE file.
+#
+# Removal or modification of this copyright notice is prohibited.
+
+module ::Axentro::Interface
+  module Helps
+    HELP_WALLET_PATH                      = I18n.translate("help.wallet.path")
+    HELP_WALLET_PATH_OR_ADDRESS_OR_DOMAIN = I18n.translate("help.wallet.some_path")
+    HELP_WALLET_ALREADY_EXISTS            = I18n.translate("help.wallet.exists")
+    HELP_WALLET_PASSWORD                  = I18n.translate("help.wallet.password")
+    HELP_WALLET_MUST_BE_ENCRYPTED         = I18n.translate("help.wallet.encrypted")
+    HELP_ADDRESS_DOMAIN_RECIPIENT         = I18n.translate("help.address")
+    HELP_NODE_ADDRESS                     = I18n.translate("help.node.address")
+    HELP_AMOUNT                           = I18n.translate("help.amount")
+    HELP_CONNECTING_NODE                  = I18n.translate("help.node.connecting")
+    HELP_BLOCK_INDEX_OR_ADDRESS           = I18n.translate("help.block.address")
+    HELP_BLOCK_INDEX_OR_TRANSACTION_ID    = I18n.translate("help.block.transaction")
+    HELP_TRANSACTION_ID                   = I18n.translate("help.transaction")
+    HELP_PUBLIC_URL                       = I18n.translate("help.url")
+    HELP_FEE                              = I18n.translate("help.fee")
+    HELP_PRICE                            = I18n.translate("help.price")
+    HELP_DOMAIN                           = I18n.translate("help.domain")
+    HELP_LOOKUP_ADDRESS                   = I18n.translate("help.lookup_address")
+    HELP_TOKEN                            = I18n.translate("help.token.name")
+    HELP_TOKEN_AMOUNT                     = I18n.translate("help.token.amount")
+    HELP_CONFIG_NAME                      = I18n.translate("help.config")
+    HELP_NODE_ID                          = I18n.translate("help.node.id")
+    HELP_DEVELOPER_FUND                   = I18n.translate("help.developer_fund")
+    HELP_IS_FAST_TRANSACTION              = I18n.translate("help.is_fast_transaction")
+    HELP_ASSET_ID                         = I18n.translate("help.assets.id")
+    HELP_ASSET_NAME                       = I18n.translate("help.assets.name")
+    HELP_ASSET_DESCRIPTION                = I18n.translate("help.assets.desc")
+    HELP_ASSET_MEDIA_LOCATION             = I18n.translate("help.assets.media_location")
+    HELP_ASSET_AMOUNT                     = I18n.translate("help.assets.amount")
+  end
+end
