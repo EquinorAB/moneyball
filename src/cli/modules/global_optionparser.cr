268e5f5a1c5010c20ba5ac405987919eff14c0df
EquinorAB <104419574+EquinorAB@users.noreply.github.com>
2021-04-21 15:12:28 +0000

    Source Formatting

--- src/cli/modules/global_optionparser.cr
+++ src/cli/modules/global_optionparser.cr
@@ -12,4 +12,62 @@
 
 module ::Axentro::Interface
   class GlobalOptionParser
-    @@instance : GlobalO
+    @@instance : GlobalOptionParser? = nil
+
+    def self.op : GlobalOptionParser
+      @@instance ||= GlobalOptionParser.new
+      @@instance.not_nil!
+    end
+
+    @connect_node : String?
+    @wallet_path : String?
+    @wallet_password : String?
+
+    @is_testnet : Bool = false
+    @is_testnet_changed = false
+    @is_private : Bool = false
+    @is_private_changed = false
+    @json : Bool = false
+
+    @bind_host : String = "0.0.0.0"
+    @bind_port : Int32 = 3000
+    @public_url : String?
+    @database_path : String?
+    @max_miners : Int32 = 512
+    @max_nodes : Int32 = 512
+
+    @address : String?
+    @amount : String?
+    @action : String?
+    @message : String = ""
+    @block_index : Int32?
+    @transaction_id : String?
+    @fee : String?
+
+    @header : Bool = false
+
+    @processes : Int32 = 1
+
+    @encrypted : Bool = false
+    @seed : String?
+    @derivation : String?
+
+    @price : String?
+    @domain : String?
+
+    @token : String?
+
+    @config_name : String?
+
+    @node_id : String?
+
+    @developer_fund_path : String?
+    @fastnode : Bool = false
+    @official_nodes_path : String?
+    @exit_if_unofficial : Bool = false
+
+    @security_level_percentage = 20_i64
+    @sync_chunk_size = 100
+
+    @is_fast_transaction : Bool = false
+    @record_nonces : Bool 
