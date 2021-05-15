529ea7fa71901c3c2f3f941f4fba83a70d2c9a24
EquinorAB <104419574+EquinorAB@users.noreply.github.com>
2021-05-15 10:29:57 +0000

    updated contents

--- src/core/blockchain/domain_model/block.cr
+++ src/core/blockchain/domain_model/block.cr
@@ -3,4 +3,48 @@
 # See the LICENSE file at the top-level directory of this distribution
 # for licensing information.
 #
-# Unless otherwise agreed in a custom licensing agreement with the Axen
+# Unless otherwise agreed in a custom licensing agreement with the Axentro Core developers,
+# no part of this software, including this file, may be copied, modified,
+# propagated, or distributed except according to the terms contained in the
+# LICENSE file.
+#
+# Removal or modification of this copyright notice is prohibited.
+module ::Axentro::Core
+  alias Chain = Array(Block)
+
+  enum BlockKind
+    SLOW
+    FAST
+
+    def to_json(j : JSON::Builder)
+      j.string(to_s)
+    end
+  end
+
+  class Block
+    extend Hashes
+
+    include JSON::Serializable
+    property index : Int64
+    property transactions : Array(Transaction)
+    property nonce : BlockNonce
+    property prev_hash : String
+    property merkle_tree_root : String
+    property timestamp : Int64
+    property difficulty : Int32
+    property kind : BlockKind
+    property address : String
+    property public_key : String
+    property signature : String
+    property hash : String
+    property version : BlockVersion
+    property hash_version : HashVersion
+    property checkpoint : String
+    property mining_version : MiningVersion
+
+    # full
+    def initialize(
+      @index : Int64,
+      @transactions : Array(Transaction),
+      @nonce : BlockNonce,
+      @prev_hash 
