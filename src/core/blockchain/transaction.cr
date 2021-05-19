994dfd3532e7cc0575a2e5cc4cc149c41c112be7
EquinorAB <104419574+EquinorAB@users.noreply.github.com>
2021-05-19 11:25:39 +0000

    Created folder remotely

--- src/core/blockchain/transaction.cr
+++ src/core/blockchain/transaction.cr
@@ -20,4 +20,33 @@ module ::Axentro::Core
       action: String,
       senders: Array(Sender),
       recipients: Recipients,
-    
+      assets: Array(Asset),
+      modules: Array(Module),
+      inputs: Array(Input),
+      outputs: Array(Output),
+      linked: String,
+      message: String,
+      token: String,
+      prev_hash: String,
+      timestamp: Int64,
+      scaled: Int32,
+      kind: TransactionKind,
+      version: TransactionVersion
+    )
+
+    setter prev_hash : String
+    @common_validated : Bool = false
+
+    def to_json(j : JSON::Builder)
+      sorted_senders = @senders.sort_by { |s| {s.address, s.public_key, s.amount, s.fee, s.signature, s.asset_id || "", s.asset_quantity || 0} }
+      sorted_recipients = @recipients.sort_by { |r| {r.address, r.amount, r.asset_id || "", r.asset_quantity || 0} }
+      sorted_assets = @assets.sort_by { |a| {a.timestamp, a.asset_id} }
+      sorted_modules = @modules.sort_by { |a| {a.timestamp, a.module_id} }
+      sorted_inputs = @inputs.sort_by { |a| {a.timestamp, a.input_id} }
+      sorted_outputs = @outputs.sort_by { |a| {a.timestamp, a.output_id} }
+      j.object do
+        j.field("id", @id)
+        j.field("action", @action)
+        j.field("message", @message)
+        j.field("token", @token)
+       
