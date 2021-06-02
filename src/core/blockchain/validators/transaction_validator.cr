5cfc5995542ebf032f1125070a0cf2d45c5d2301
EquinorAB <104419574+EquinorAB@users.noreply.github.com>
2021-06-02 15:10:08 +0000

    sidebar

--- src/core/blockchain/validators/transaction_validator.cr
+++ src/core/blockchain/validators/transaction_validator.cr
@@ -32,4 +32,27 @@ module ::Axentro::Core::TransactionValidator
     blockchain.dapps.each do |dapp|
       related_transactions = vt.passed.select { |t| dapp.transaction_related?(t.action) }
       if related_transactions.size > 0
-        vt.concat(dapp.valid
+        vt.concat(dapp.valid?(related_transactions))
+      end
+    end
+
+    vt
+  end
+
+  def validate_senders(transaction : Axentro::Core::Transaction, network_type : String)
+    transaction.senders.each do |sender|
+      network = Keys::Address.from(sender.address, "sender").network
+      return FailedTransaction.new(transaction, "sender address: #{sender.address} has wrong network type: #{network[:name]}, this node is running as: #{network_type}") if network[:name] != network_type
+
+      public_key = Keys::PublicKey.new(sender.public_key, network)
+
+      if public_key.address.as_hex != sender.address
+        return FailedTransaction.new(transaction, "sender public key mismatch - sender public key: #{public_key.as_hex} is not for sender address: #{sender.address}")
+      end
+
+      verbose "unsigned_json: #{transaction.as_unsigned.to_json}"
+      verbose "unsigned_json_hash: #{transaction.as_unsigned.to_hash}"
+      verbose "public key: #{public_key.as_hex}"
+      verbose "signature: #{sender.signature}"
+
+      verify_result = KeyUtils.verify_signature(transaction.as_unsigned.to_hash, sender.signature, p
