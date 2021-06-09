59d1cf77362efccce5f88f783fd6cc30f5569562
EquinorAB <104419574+EquinorAB@users.noreply.github.com>
2021-06-09 21:02:59 +0000

    auto@ignore

--- src/core/dapps/build_in/nonce_info.cr
+++ src/core/dapps/build_in/nonce_info.cr
@@ -17,4 +17,50 @@ module ::Axentro::Core::DApps::BuildIn
     property nonce : String
     property latest_hash : String
     property block_id : Int64
-    property
+    property difficulty : Int32
+    property timestamp : Int64
+
+    def initialize(@address : String, @nonce : String, @latest_hash : String, @block_id : Int64, @difficulty : Int32, @timestamp : Int64); end
+  end
+
+  class NonceInfo < DApp
+    def setup
+    end
+
+    def transaction_actions : Array(String)
+      [] of String
+    end
+
+    def transaction_related?(action : String) : Bool
+      false
+    end
+
+    def valid_transactions?(transactions : Array(Transaction)) : ValidatedTransactions
+      ValidatedTransactions.passed(transactions)
+    end
+
+    def record(chain : Blockchain::Chain)
+    end
+
+    def clear
+    end
+
+    def define_rpc?(call, json, context, params) : HTTP::Server::Context?
+      nil
+    end
+
+    def nonces(json, context, params) : Array(Nonce)
+      address = json["address"].as_s
+      block_id = json["block_id"].as_i64
+      context.response.print api_success(nonces_impl(address, block_id))
+      context
+    end
+
+    def pending_nonces(json, context, params) : Array(Nonce)
+      address = json["address"].as_s
+      context.response.print api_success(pending_nonces_impl(address))
+      context
+    end
+
+    def nonces_impl(address : String, block_id : Int64) : Array(Nonce)
+   
