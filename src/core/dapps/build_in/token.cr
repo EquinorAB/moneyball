f5407dec623e944daf69910fff841ec49f9cfb02
EquinorAB <104419574+EquinorAB@users.noreply.github.com>
2021-06-11 12:09:27 +0000

    v0.7.7

--- src/core/dapps/build_in/token.cr
+++ src/core/dapps/build_in/token.cr
@@ -11,3 +11,40 @@
 # Removal or modification of this copyright notice is prohibited.
 
 module ::Axentro::Core::DApps::BuildIn
+  struct TokenInfo
+    getter created_by : String
+    getter is_locked : Bool
+
+    def initialize(@created_by : String, @is_locked : Bool)
+    end
+  end
+
+  class Token < DApp
+    getter tokens : Array(String) = ["AXNT"]
+
+    def setup
+    end
+
+    def transaction_actions : Array(String)
+      ["create_token", "update_token", "lock_token", "burn_token"]
+    end
+
+    def transaction_related?(action : String) : Bool
+      transaction_actions.includes?(action)
+    end
+
+    # split these out to reduce complexity in the future
+    # ameba:disable Metrics/CyclomaticComplexity
+    def valid_transactions?(transactions : Array(Transaction)) : ValidatedTransactions
+      vt = ValidatedTransactions.empty
+      processed_transactions = transactions.select(&.is_coinbase?)
+
+      body_transactions = transactions.reject(&.is_coinbase?)
+      token_map = database.token_info(body_transactions.map(&.token).reject(&.==(TOKEN_DEFAULT)).uniq!)
+
+      body_transactions.each do |transaction|
+        token = transaction.token
+        action = transaction.action
+
+        # common rules for token
+        raise "must not be the default token: #{token}" if t
