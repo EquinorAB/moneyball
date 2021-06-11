ccac0f950d6b080232b529bfdd84e5690744bfe4
EquinorAB <104419574+EquinorAB@users.noreply.github.com>
2021-06-11 08:57:13 +0000

    lil fix

--- src/core/dapps/build_in/official_node.cr
+++ src/core/dapps/build_in/official_node.cr
@@ -15,4 +15,44 @@ module ::Axentro::Core::DApps::BuildIn
     def setup
     end
 
-    def transaction_a
+    def transaction_actions : Array(String)
+      ["create_official_node_slow", "create_official_node_fast"]
+    end
+
+    def transaction_related?(action : String) : Bool
+      transaction_actions.includes?(action)
+    end
+
+    def valid_transactions?(transactions : Array(Transaction)) : ValidatedTransactions
+      ValidatedTransactions.with(
+        transactions.select { |t| transaction_actions.includes?(t.action) },
+        "Creation of official nodes is not currently permitted. Future changes will enable this.",
+        transactions.reject { |t| transaction_actions.includes?(t.action) }
+      )
+    end
+
+    def record(chain : Blockchain::Chain)
+    end
+
+    def clear
+    end
+
+    def define_rpc?(call, json, context, params) : HTTP::Server::Context?
+      nil
+    end
+
+    def all_impl : Array(String)
+      database.get_official_nodes.values.flatten.uniq!
+    end
+
+    def all_slow_impl : Array(String)
+      database.get_official_nodes["slownodes"].uniq
+    end
+
+    def all_fast_impl : Array(String)
+      database.get_official_nodes["fastnodes"].uniq
+    end
+
+    def i_am_a_fastnode?(address : String) : Bool
+      all_fast_impl.includes?(address)
+    en
