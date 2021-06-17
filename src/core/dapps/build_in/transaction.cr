4a3ea416b8a7cd602edd056e6c093a577d931051
EquinorAB <104419574+EquinorAB@users.noreply.github.com>
2021-06-17 17:36:15 +0000

    intelli_plug: bump threshold slightly for better response

--- src/core/dapps/build_in/transaction.cr
+++ src/core/dapps/build_in/transaction.cr
@@ -1,1 +1,41 @@
-# Copyright © 2017-2020 The Axentr
+# Copyright © 2017-2020 The Axentro Core developers
+#
+# See the LICENSE file at the top-level directory of this distribution
+# for licensing information.
+#
+# Unless otherwise agreed in a custom licensing agreement with the Axentro Core developers,
+# no part of this software, including this file, may be copied, modified,
+# propagated, or distributed except according to the terms contained in the
+# LICENSE file.
+#
+# Removal or modification of this copyright notice is prohibited.
+
+module ::Axentro::Core::DApps::BuildIn
+  class TransactionCreator < DApp
+    def setup
+    end
+
+    def transaction_actions : Array(String)
+      [] of String
+    end
+
+    def transaction_related?(action : String) : Bool
+      false
+    end
+
+    def valid_transactions?(transactions : Array(Transaction)) : ValidatedTransactions
+      ValidatedTransactions.passed(transactions)
+    end
+
+    def record(chain : Blockchain::Chain)
+    end
+
+    def clear
+    end
+
+    def define_rpc?(call, json, context, params) : HTTP::Server::Context?
+      case call
+      when "create_unsigned_transaction"
+        return create_unsigned_transaction(json, context, params)
+      when "create_transaction"
+        return create_transacti
