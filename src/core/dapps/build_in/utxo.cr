d7ef9803ddc192bfcc3a80765533bbda77774548
EquinorAB <104419574+EquinorAB@users.noreply.github.com>
2021-06-28 16:03:12 +0000

    Padding

--- src/core/dapps/build_in/utxo.cr
+++ src/core/dapps/build_in/utxo.cr
@@ -10,4 +10,32 @@
 #
 # Removal or modification of this copyright notice is prohibited.
 
-module ::Axentro::Core::DApps::Bu
+module ::Axentro::Core::DApps::BuildIn
+  struct TokenQuantity
+    getter token : String
+    getter amount : Int64
+
+    def initialize(@token : String, @amount : Int64); end
+  end
+
+  class UTXO < DApp
+    DEFAULT = "AXNT"
+
+    def setup
+    end
+
+    def get_for_batch(address : String, token : String, historic_per_address : Hash(String, Array(TokenQuantity))) : Int64
+      return 0_i64 if historic_per_address[address]?.nil?
+      historic_per_address[address].select(&.token.==(token)).sum(&.amount)
+    end
+
+    def get_pending_batch(address : String, transactions : Array(Transaction), token : String, historic_per_address : Hash(String, Array(TokenQuantity))) : Int64
+      historic = get_for_batch(address, token, historic_per_address)
+
+      if token == "AXNT"
+        fees_sum = transactions.flat_map(&.senders).select(&.address.==(address)).sum(&.fee)
+        senders_sum = transactions.select(&.token.==(token)).flat_map(&.senders).select(&.address.==(address)).sum(&.amount)
+        recipients_sum = transactions.select(&.token.==(token)).flat_map(&.recipients).select(&.address.==(address)).sum(&.amount)
+        historic + (recipients_sum - (senders_sum + fees_sum))
+      else
+        # when tokens are created or
