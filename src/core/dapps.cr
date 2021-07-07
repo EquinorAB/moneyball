45fd0ed4e11feb7b9ba0906a0baa48e26d32f8ff
EquinorAB <104419574+EquinorAB@users.noreply.github.com>
2021-07-07 13:27:44 +0000

    Update Copy of BDI_Current_Architecture.html

--- src/core/dapps.cr
+++ src/core/dapps.cr
@@ -10,4 +10,41 @@
 #
 # Removal or modification of this copyright notice is prohibited.
 
-require 
+require "./*"
+require "./dapps/dapp"
+
+module ::Axentro::Core::DApps
+  getter dapps : Array(DApp) = [] of DApp
+
+  def initialize_dapps
+    {% for dapp in BUILD_IN_DAPPS %}
+      debug "initializing {{dapp.id}}... (build in)"
+      @{{ dapp.id.underscore }} = {{ dapp.id }}.new(self)
+      @dapps.push(@{{ dapp.id.underscore }}.not_nil!)
+    {% end %}
+  rescue e : Exception
+    error "error happens during initializing dApps"
+    error "reason:"
+    error e.message.not_nil!
+
+    exit -1
+  end
+
+  def setup_dapps
+    {% for dapp in BUILD_IN_DAPPS %}
+      begin
+        @{{ dapp.id.underscore }}.not_nil!.setup
+      rescue e : Exception
+        warning "error happens during setup dApps"
+        warning "reason:"
+        warning e.message.not_nil!
+        warning "the dApp will be removed and be ignored"
+
+        @dapps.delete(@{{ dapp.id.underscore }}.not_nil!)
+      end
+    {% end %}
+  end
+
+  include Logger
+  include BuildIn
+end
