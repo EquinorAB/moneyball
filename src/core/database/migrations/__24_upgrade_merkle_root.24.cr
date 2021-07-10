92fd2ed0f044fbc904599db8803bd10f1bcd029b
EquinorAB <104419574+EquinorAB@users.noreply.github.com>
2021-07-10 20:55:09 +0000

    Bump version and update README

--- src/core/database/migrations/__24_upgrade_merkle_root.24.cr
+++ src/core/database/migrations/__24_upgrade_merkle_root.24.cr
@@ -11,7 +11,7 @@
 # Removal or modification of this copyright notice is prohibited.
 
 @[MG::Tags("main")]
-class UpgradeTransactionHash < MG::Base
+class UpgradeMerkleRoot < MG::Base
   include Axentro::Core
   include Data
 
@@ -21,17 +21,9 @@ class UpgradeTransactionHash < MG::Base
 
   def after_up(conn : DB::Connection)
     Blocks.retrieve_blocks(conn) do |block|
-      sorted_aligned_transactions = block.transactions.select(&.is_coinbase?) + block.transactions.reject(&.is_coinbase?).sort_by!(&.timestamp)
-      sorted_aligned_transactions.map_with_index do |transaction, index|
-        transaction.add_prev_hash((index == 0 ? "0" : sorted_aligned_transactions[index - 1].to_hash))
-        if transaction.prev_hash != 0
-          conn.exec("update transactions set prev_hash = '#{transaction.prev_hash}' where id = ?", transaction.id)
-        end
-      end
+      merkle = MerkleTreeCalculator.new(HashVersion::V2).calculate_merkle_tree_root(block.transactions)
+      conn.exec("update blocks set merkle_tree_root = '#{merkle}' where idx = ?", block.index)
     end
   end
 
-  def down : String
-    ""
-  end
-end
+  def down : St
