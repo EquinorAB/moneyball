3859ef10ba18a7541874b9f85c682a60fffae926
EquinorAB <104419574+EquinorAB@users.noreply.github.com>
2021-07-12 21:15:59 +0000

    remove some logging

--- src/core/database/migrations/__5_create_rejects_table.5.cr
+++ src/core/database/migrations/__5_create_rejects_table.5.cr
@@ -1,3 +1,4 @@
+
 # Copyright © 2017-2020 The Axentro Core developers
 #
 # See the LICENSE file at the top-level directory of this distribution
@@ -11,26 +12,21 @@
 # Removal or modification of this copyright notice is prohibited.
 
 @[MG::Tags("main")]
-class CreateSendersTable < MG::Base
+class CreateRejectsTable < MG::Base
   def up : String
     <<-SQL
-    CREATE TABLE IF NOT EXISTS senders (
-      transaction_id      TEXT NOT NULL,
-      block_id            INTEGER NOT NULL,
-      idx                 INTEGER NOT NULL,
+    CREATE TABLE IF NOT EXISTS rejects (
+      transaction_id      TEXT PRIMARY KEY,
       address             TEXT NOT NULL,
-      public_key          TEXT NOT NULL,
-      amount              INTEGER NOT NULL,
-      fee                 INTEGER NOT NULL,
-      signature           TEXT NOT NULL,
-      primary key         (transaction_id, block_id, idx)
+      reason              TEXT NOT NULL,
+      timestamp           INTEGER NOT NULL
       );
     SQL
   end
 
   def down : String
     <<-SQL
-      DROP TABLE senders;
+      DROP TABLE rejects;
     SQL
   end
 end
