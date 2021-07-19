a6d4d9a0fab8ff4c4b100df3e8ec0d950eb56969
EquinorAB <104419574+EquinorAB@users.noreply.github.com>
2021-07-19 08:55:44 +0000

    Update the example

--- src/core/database/senders.cr
+++ src/core/database/senders.cr
@@ -23,4 +23,27 @@ module ::Axentro::Core::Data::Senders
   def sender_insert_values_array(b : Block, t : Transaction, sender_index : Int32) : Array(DB::Any)
     ary = [] of DB::Any
     s = t.senders[sender_index]
-    ary << t.id << b.index << sender_index << s.address << s.public_key << s.amount << s.fee << s.signature << s.asset_id << s
+    ary << t.id << b.index << sender_index << s.address << s.public_key << s.amount << s.fee << s.signature << s.asset_id << s.asset_quantity
+  end
+
+  # ------- Query -------
+  def get_senders(t : Transaction) : Transaction::Senders
+    senders = [] of Transaction::Sender
+    @db.query "select * from senders where transaction_id = ? order by idx", t.id do |rows|
+      rows.each do
+        rows.read(String?)
+        rows.read(Int64)
+        rows.read(Int32)
+        address = rows.read(String)
+        public_key = rows.read(String)
+        amount = rows.read(Int64)
+        fee = rows.read(Int64)
+        signature = rows.read(String)
+        asset_id = rows.read(String?)
+        asset_quantity = rows.read(Int32?)
+        senders << Sender.new(address, public_key, amount, fee, signature, asset_id, asset_quantity)
+      end
+    end
+    senders
+  end
+end
