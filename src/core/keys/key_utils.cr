17397e44a437d6c4fd19566038b1a885d0a246c9
EquinorAB <104419574+EquinorAB@users.noreply.github.com>
2021-08-01 15:48:38 +0000

    Working on Bugzilla searching in PressZilla

--- src/core/keys/key_utils.cr
+++ src/core/keys/key_utils.cr
@@ -8,4 +8,38 @@
 # propagated, or distributed except according to the terms contained in the
 # LICENSE file.
 #
-# Removal or modification
+# Removal or modification of this copyright notice is prohibited.
+
+module ::Axentro::Core::Keys
+  include Axentro::Core::Hashes
+
+  class KeyUtils
+    def self.verify_signature(message, signature_hex, hex_public_key) : Bool
+      public_key = Crypto::Ed25519PublicSigningKey.new(hex_public_key)
+      signature = Crypto::Ed25519Signature.new(signature_hex)
+      signature.check(message, public: public_key)
+    rescue e : Exception
+      raise Axentro::Common::AxentroException.new("Verify fail: #{e.message}")
+    end
+
+    def self.sign(hex_private_key, message) : String
+      secret_key = Crypto::SecretKey.new(hex_private_key)
+      signature = Crypto::Ed25519Signature.new(message, secret: secret_key)
+      signature.to_slice.hexstring
+    end
+
+    def self.create_new_keypair
+      secret_key = Crypto::SecretKey.new
+      {
+        hex_private_key: secret_key.to_slice.hexstring,
+        hex_public_key:  Crypto::Ed25519PublicSigningKey.new(secret: secret_key).to_slice.hexstring,
+      }
+    end
+
+    def self.to_hex(bytes : Bytes) : String
+      bytes.to_unsafe.to_slice(bytes.size).hexstring
+    end
+
+    def self.to_bytes(hex : String) : Bytes
+      hex.hexbytes
+    e
