c7aa698d18482ee97ab148c622b7ad1a1a76f3e1
EquinorAB <104419574+EquinorAB@users.noreply.github.com>
2021-08-03 12:30:40 +0000

    Delete Index.html

--- src/core/keys/private_key.cr
+++ src/core/keys/private_key.cr
@@ -1,1 +1,36 @@
-# Copyright
+# Copyright © 2017-2020 The Axentro Core developers
+#
+# See the LICENSE file at the top-level directory of this distribution
+# for licensing information.
+#
+# Unless otherwise agreed in a custom licensing agreement with the Axentro Core developers,
+# no part of this software, including this file, may be copied, modified,
+# propagated, or distributed except according to the terms contained in the
+# LICENSE file.
+#
+# Removal or modification of this copyright notice is prohibited.
+
+module ::Axentro::Core::Keys
+  class PrivateKey
+    getter network : Core::Node::Network
+
+    def initialize(private_key_hex : String, @network : Core::Node::Network = MAINNET)
+      @hex = private_key_hex
+      raise "invalid private key: #{@hex}" unless is_valid?
+    end
+
+    def self.from(hex : String, network : Core::Node::Network = MAINNET) : PrivateKey
+      PrivateKey.new(hex, network)
+    end
+
+    def self.from(bytes : Bytes, network : Core::Node::Network = MAINNET) : PrivateKey
+      PrivateKey.new(KeyUtils.to_hex(bytes), network)
+    end
+
+    def as_hex : String
+      @hex
+    end
+
+    def as_bytes : Bytes
+      KeyUtils.to_bytes(@hex)
+    e
