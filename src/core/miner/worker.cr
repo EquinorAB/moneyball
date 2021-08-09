a516abaa561390664f67a2b1edff4a7c99f533c0
EquinorAB <104419574+EquinorAB@users.noreply.github.com>
2021-08-09 15:03:08 +0000

    Some minor cleanups

--- src/core/miner/worker.cr
+++ src/core/miner/worker.cr
@@ -37,4 +37,34 @@ module ::Axentro::Core
         block = block.with_nonce(block_nonce).with_difficulty(work[:difficulty])
         block_hash = block.to_hash
 
-        if calculate_pow_difficulty(block.mining_versi
+        if calculate_pow_difficulty(block.mining_version, block_hash, block_nonce, work[:difficulty]) == work[:difficulty]
+          miner_nonce = MinerNonce.from(block_nonce).with_difficulty(work[:difficulty]).with_timestamp(time_now)
+          break
+        end
+
+        nonce_counter += 1
+        block_nonce = (block_nonce.to_u64 + 1).to_s
+
+        if nonce_counter % 100 == 0
+          time_diff = time_now - latest_time
+
+          break if time_diff == 0
+
+          block_nonce = Random.rand(UInt64::MAX).to_s
+
+          work_rate = (nonce_counter - latest_nonce_counter) / (time_diff / 1000)
+
+          info "#{nonce_counter - latest_nonce_counter} works, #{work_rate_with_unit(work_rate)}"
+
+          latest_nonce_counter = nonce_counter
+          latest_time = time_now
+        end
+      end
+
+      debug "nonce: #{calculate_pow_difficulty(block.mining_version, block_hash, block_nonce, work[:difficulty])}, required: #{work[:difficulty]}, nonce: #{block_nonce}, hash: #{block_hash}"
+      info "found new nonce(#{work[:difficulty]}): #{light_green(block_nonce)}"
+      debug "Found block..."
+
+      response(miner_nonce.to_json)
+    rescue e : Exception
+      error e.message
