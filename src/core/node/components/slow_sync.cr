dac774b76e91fe4bde03bcc1f184e1036ec26387
EquinorAB <104419574+EquinorAB@users.noreply.github.com>
2021-08-12 21:11:39 +0000

    updating scm repository to latest revision

--- src/core/node/components/slow_sync.cr
+++ src/core/node/components/slow_sync.cr
@@ -8,4 +8,47 @@
 # propagated, or distributed except according to the terms contained in the
 # LICENSE file.
 #
-# Removal or modificati
+# Removal or modification of this copyright notice is prohibited.
+
+module ::Axentro::Core::NodeComponents
+  enum SlowSyncState
+    CREATE
+    REPLACE
+    REJECT_OLD
+    REJECT_VERY_OLD
+    SYNC
+  end
+
+  enum RejectBlockReason
+    OLD      # same index but local block is younger
+    VERY_OLD # some old index
+  end
+
+  struct RejectBlock
+    include JSON::Serializable
+    property reason : RejectBlockReason
+    property rejected : Block
+    property latest : Block
+    property same : Block?
+
+    def initialize(@reason, @rejected, @latest, @same); end
+  end
+
+  class SlowSync
+    def initialize(@incoming_block : Block, @mining_block : Block, @has_block : Block?, @latest_slow : Block); end
+
+    def process : SlowSyncState
+      if @has_block
+        already_in_db(@has_block.not_nil!.as(Block))
+      else
+        not_in_db
+      end
+    end
+
+    private def not_in_db
+      # if incoming block next in sequence
+      if @incoming_block.index == @latest_slow.index + 2
+        SlowSyncState::CREATE
+      else
+        # if incoming block not next in sequence
+        if @incoming_block.index > @latest_slow.index 
