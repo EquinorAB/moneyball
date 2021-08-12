a2b89ab700875caa8779c947abfc8c30c96102e8
EquinorAB <104419574+EquinorAB@users.noreply.github.com>
2021-08-12 19:57:06 +0000

    Copyright year.

--- src/core/node/components/sem_ver.cr
+++ src/core/node/components/sem_ver.cr
@@ -19,4 +19,18 @@ module ::Axentro::Core::NodeComponents
     def initialize(@version : String)
       info = validate
       @major_version = info[:major_version]
-      @minor_version = info[:m
+      @minor_version = info[:minor_version]
+      @patch_version = info[:patch_version]
+    end
+
+    private def validate
+      semver_regex = /^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$/
+      raise "Invalid sementic version format for supplied version: #{@version} - it should be in the format e.g. 1.0.1" unless semver_regex.match(@version)
+
+      data = semver_regex.match(@version).not_nil!
+      {major_version: data[1].to_i, minor_version: data[2].to_i, patch_version: data[3].to_i}
+    rescue e : Exception
+      raise "Semantic versioning validation error: #{e.message || "unknown"}"
+    end
+  end
+end
