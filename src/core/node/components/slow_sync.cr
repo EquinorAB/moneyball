d9f686153db08294de3ca670367e0c3850c0ae0e
EquinorAB <104419574+EquinorAB@users.noreply.github.com>
2021-08-13 09:46:55 +0000

    Changed class name

--- src/core/node/components/slow_sync.cr
+++ src/core/node/components/slow_sync.cr
@@ -51,4 +51,30 @@ module ::Axentro::Core::NodeComponents
         SlowSyncState::CREATE
       else
         # if incoming block not next in sequence
-        if @incoming_block.index > @latest_slow.index 
+        if @incoming_block.index > @latest_slow.index + 2
+          # incoming is ahead of next in sequence
+          SlowSyncState::SYNC
+        else
+          # incoming is behind next in sequence (can ignore this block)
+          SlowSyncState::REJECT_VERY_OLD
+        end
+      end
+    end
+
+    private def already_in_db(existing_block : Block)
+      # if incoming block latest in sequence
+      if @incoming_block.index == @latest_slow.index
+        if @incoming_block.difficulty > existing_block.difficulty
+          # incoming block has higher difficulty than our (take theirs)
+          SlowSyncState::REPLACE
+        elsif @incoming_block.difficulty < existing_block.difficulty
+          # our block has higher difficulty than incoming one (keep ours & ignore)
+          SlowSyncState::REJECT_OLD
+        else
+          @incoming_block.difficulty == existing_block.difficulty
+          # incoming block and our block are the same difficulty - choose by earliest timestamp
+          if @incoming_block.timestamp < existing_block.timestamp
+            # incoming block is earlier then ours (take theirs)
+            SlowSyncState::REPLACE
+          elsif @incoming_block.timestamp > existing_block.timestamp
+            # i
