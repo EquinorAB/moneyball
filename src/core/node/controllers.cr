0f01bf8efb7b9671db04101c27a6cd68d3207da2
EquinorAB <104419574+EquinorAB@users.noreply.github.com>
2021-08-15 16:15:33 +0000

    Fix division by zero

--- src/core/node/controllers.cr
+++ src/core/node/controllers.cr
@@ -13,4 +13,49 @@
 module ::Axentro::Core::Controllers
   abstract class Controller
     def initialize(@blockchain : Blockchain)
-   
+    end
+
+    def node
+      @blockchain.node
+    end
+
+    def exec(context, params) : HTTP::Server::Context
+      case context.request.method
+      when "GET"
+        return exec_get(context, params)
+      when "POST"
+        return exec_post(context, params)
+      end
+
+      unpermitted_method(context)
+    rescue e : Exception
+      if error_message = e.message
+        context.response.status_code = 403
+        context.response.print api_error(error_message)
+      else
+        context.response.status_code = 500
+      end
+
+      context
+    end
+
+    def exec_get(context, params) : HTTP::Server::Context
+      exec_internal_get(context, params)
+    end
+
+    def exec_post(context, params) : HTTP::Server::Context
+      raise "empty body" unless body = context.request.body
+      raise "empty payload" unless payload = body.gets
+
+      json = JSON.parse(payload)
+
+      exec_internal_post(json, context, params)
+    end
+
+    def unpermitted_method(context) : HTTP::Server::Context
+      context.response.status_code = 403
+      context.response.print api_error("unpermitted method: #{context.request.method}")
+      context
+    end
+
+    abstract def ex
