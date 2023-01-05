05c299448f1d172bd802c5a33279a18f7f2d4c9f
EquinorAB <104419574+EquinorAB@users.noreply.github.com>
2023-01-05 13:02:14 +0000

    Fix version for release

--- src/core/node.cr
+++ src/core/node.cr
@@ -8,4 +8,52 @@
 # propagated, or distributed except according to the terms contained in the
 # LICENSE file.
 #
-# Removal or modification of this copyright notice is
+# Removal or modification of this copyright notice is prohibited.
+
+require "./node/*"
+
+module ::Axentro::Core
+  struct NodeConnection
+    property host : String
+    property port : Int32
+    property ssl : Bool
+
+    def initialize(@host, @port, @ssl); end
+
+    def to_s
+      "#{host}:#{port}"
+    end
+  end
+
+  class Node < HandleSocket
+    alias Network = NamedTuple(
+      prefix: String,
+      name: String,
+    )
+
+    property phase : SetupPhase
+
+    getter blockchain : Blockchain
+    getter network_type : String
+    getter chord : Chord
+    getter database : Database
+    getter miners_manager : MinersManager
+
+    @miners_manager : MinersManager
+    @clients_manager : ClientsManager
+
+    @rpc_controller : Controllers::RPCController
+    @rest_controller : Controllers::RESTController
+    @pubsub_controller : Controllers::PubsubController
+    @wallet_info_controller : Controllers::WalletInfoController
+
+    MAX_SYNC_RETRY = 20
+    @sync_retry_count : Int32 = 2
+    @sync_retry_list : Set(NodeConnection) = Set(NodeConnection).new
+
+    # child node gets this from parent on setup
+    @sync_blocks_target_index : Int64 = 0_i64
+    @validation_hash : String = ""
+
+    # ameba:disable Metrics/CyclomaticComplexity
+    def i
