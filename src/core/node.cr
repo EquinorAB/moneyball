a4a1eff2080dbe0460036cb1faa279601ec4c034
EquinorAB <104419574+EquinorAB@users.noreply.github.com>
2023-01-06 16:20:59 +0000

    Update: README

--- src/core/node.cr
+++ src/core/node.cr
@@ -56,4 +56,37 @@ module ::Axentro::Core
     @validation_hash : String = ""
 
     # ameba:disable Metrics/CyclomaticComplexity
-    def i
+    def initialize(
+      @is_private : Bool,
+      @is_testnet : Bool,
+      @bind_host : String,
+      @bind_port : Int32,
+      @public_host : String?,
+      @public_port : Int32?,
+      @ssl : Bool?,
+      @connect_host : String?,
+      @connect_port : Int32?,
+      @wallet : Wallet?,
+      @wallet_address : String,
+      @database_path : String,
+      @database : Database,
+      @developer_fund : DeveloperFund?,
+      @official_nodes : OfficialNodes?,
+      @exit_on_unofficial : Bool,
+      @security_level_percentage : Int64,
+      @sync_chunk_size : Int32,
+      @record_nonces : Bool,
+      @max_miners : Int32,
+      @max_private_nodes : Int32,
+      @whitelist : Array(String),
+      @whitelist_message : String,
+      @metrics_whitelist : Array(String),
+      @use_ssl : Bool = false
+    )
+      welcome
+
+      @phase = SetupPhase::NONE
+
+      @network_type = @is_testnet ? "testnet" : "mainnet"
+      @blockchain = Blockchain.new(@network_type, @wallet, @wallet_address, @database_path, @database, @developer_fund, @official_nodes, @security_level_percentage, @sync_chunk_size, @record_nonces, @max_miners, is_standalone?)
+      @chord = Chord.new(@
