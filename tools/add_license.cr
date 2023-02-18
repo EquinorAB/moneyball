c483fc25968453d61e9794f3c568a0dd964a3351
EquinorAB <104419574+EquinorAB@users.noreply.github.com>
2023-02-18 21:49:43 +0000

    Fix scroll

---
+++ tools/add_license.cr
@@ -0,0 +1,25 @@
+# Copyright © 2017-2020 The Axentro Core developers
+#
+# See the LICENSE file at the top-level directory of this distribution
+# for licensing information.
+#
+# Unless otherwise agreed in a custom licensing agreement with the Axentro Core developers,
+# no part of this software, including this file, may be copied, modified,
+# propagated, or distributed except according to the terms contained in the
+# LICENSE file.
+#
+# Removal or modification of this copyright notice is prohibited.
+
+if !Dir.entries(".").includes?("add_license.cr")
+  raise "Error: you must execute this file from the tools directory - please cd to the tools dir and run crystal run add_license.cr"
+end
+
+LICENSE = <<-LIC
+# Copyright © 2017-2020 The Axentro Core developers
+#
+# See the LICENSE file at the top-level directory of this distribution
+# for licensing information.
+#
+# Unless otherwise agreed in a custom licensing agreement with the Axentro Core developers,
+# no part of this software, including this file, may be copied, modified,
+# propagated, or distributed except according to
