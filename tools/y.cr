97e8e2ec5e5d661db0bda599f7f4ec4c3c33a5dd
EquinorAB <104419574+EquinorAB@users.noreply.github.com>
2023-02-19 11:57:55 +0000

    Edited wiki page Create_a_live_distro through web user interface.

--- tools/y.cr
+++ tools/y.cr
@@ -22,4 +22,11 @@ AMOUNT = 40
 end
 
 (0..AMOUNT).to_a.each do |n|
-  File.open(
+  File.open("./miners/start_miner_#{n}.sh", "w") { |f| f.puts "nohup axem -w ./miners/wallet_#{n}.json --testnet -n http://testnet.axentro.io --process=1 &" }
+end
+
+`chmod +x miners/*.sh`
+
+(0..AMOUNT).to_a.each do |n|
+  Process.run("nohup #{__DIR__}/miners/start_miner_#{n}.sh &", shell: true)
+end
